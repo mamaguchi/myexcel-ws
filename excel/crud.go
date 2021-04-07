71cea3c9db9f993b93de63cf47e23088176535f3
patrickchow <patrickchow688@gmail.com>
2021-04-08 07:42:22 +0800

    Updated 7am 8-4-2021.

--- excel/crud.go
+++ excel/crud.go
@@ -16,23 +16,58 @@ import (
 
 
 type NewPersonIn struct{
+	Bil int 
+	Notifydt string
 	Name string 
 	Ident string 
 	Tel string 
 	Address string 
+	State string
 }
 
 func AddNewPerson(conn *pgxpool.Pool, npi NewPersonIn) error {
 	sql :=
 		`insert into wbk.linelisting
-		( name, ident, tel, address )
+		( bil, notifydt, name, ident, tel, address, state )
 		values
-		( $1, $2, $3, $4 )`
+		( $1, $2, $3, $4, $5, $6, $7 )`
 		
 	_, err := conn.Exec(context.Background(), sql, 
-		npi.Name, npi.Ident, npi.Tel, npi.Address)
+		npi.Bil, npi.Notifydt, npi.Name, 
+		npi.Ident, npi.Tel, npi.Address, npi.State)
 	if err != nil {
 		return err 
 	}
 	return nil 
+}
+
+func GetPersons(conn *pgxpool.Pool) (Linelisting, error) {
+	sql :=
+		`select bil, address, state
+		   from wbk.linelisting`
+
+	rows, err := conn.Query(context.Background(), sql)
+	if err != nil {
+		return nil, err
+	}
+
+	var linelisting Linelisting  
+	for rows.Next() {
+		var bil int 
+		var address string 
+		var state string 
+
+		err = rows.Scan(&bil, &address, &state)
+		if err != nil {
+			return nil, err 
+		}
+
+		person := Person{
+			Bil: bil,
+			Address: address,
+			State: state,
+		}
+		linelisting.Persons = append(linelisting.Persons, person)
+	}
+	return linelisting, err
 }
